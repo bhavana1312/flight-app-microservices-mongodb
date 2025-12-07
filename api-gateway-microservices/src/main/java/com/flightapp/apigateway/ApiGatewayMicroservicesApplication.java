522a810c2ca32e62e498c6cab08d2f397f83ceb3
bhavana1312 <bhavana13sree@gmail.com>
2025-12-07 11:52:27 +0530

    create api gateway

---
+++ api-gateway-microservices/src/main/java/com/flightapp/apigateway/ApiGatewayMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMicroservicesApplication.class, args);
+	}
+
+}
