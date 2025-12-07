c968e92fae9695bf59273a90c2218c022083abf5
bhavana1312 <bhavana13sree@gmail.com>
2025-12-07 22:48:40 +0530

    create auth service

---
+++ auth-service/src/main/java/com/flightapp/authservice/AauthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AauthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AauthServiceApplication.class, args);
+	}
+
+}
