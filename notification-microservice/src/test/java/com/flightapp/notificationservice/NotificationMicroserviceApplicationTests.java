d16a63bef961573fe5e1db0dfcd6c908a4f6ea0f
bhavana1312 <bhavana13sree@gmail.com>
2025-12-07 10:12:04 +0530

    created notification microservice

---
+++ notification-microservice/src/test/java/com/flightapp/notificationservice/NotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
